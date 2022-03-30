0694c1b1a6579e6d24e3e3c45306bab04eb6c024
Pericles Zapata de Oliveira <pzoliveira@outlook.com>
2022-03-29 23:29:40 -0300

    Project - Part 4

--- movies-control.sql
+++ movies-control.sql
@@ -21,3 +21,11 @@ INSERT INTO `movies_control`.`movies`
 `name`)
 VALUES
 (1, 'Avengers');
+INSERT INTO `movies_control`.`movies`
+(`type`,
+`name`,
+`total_ep`,
+`atual_ep`)
+VALUES
+(1, '300', NULL, NULL);
+UPDATE movies SET last_view = '2022-02-28 02:09:51' WHERE id=1 OR id=2;
